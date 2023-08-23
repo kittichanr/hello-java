4d1f71fcc8c70467e97315d1b2798b868a17c738
kittichanr <petchkubbb@gmail.com>
2023-08-23 09:52:48 +0700

    printf, final

---
+++ src/main/java/oop/OOP.java
@@ -0,0 +1,7 @@
+package oop;
+
+public class oop {
+    public static void main(String[] args) {
+        System.out.println("oop");
+    }
+}
--- src/main/java/org/hellojava/Main.java
+++ src/main/java/org/hellojava/Main.java
@@ -346,8 +346,46 @@ public class Main {
 //
 //
 // ------------------ printf -----------------------//
-//
+//      // printf() = 	an optional method to control, format, and display text to the console window
+//		//				two arguments = format string + (object/variable/value)
+//		//				% [flags] [precision] [width] [conversion-character]
 
+//        System.out.printf("This is a format string %d", 123);
+//
+//        boolean myBoolean = true;
+//        char myChar = '@';
+//        String myString = "Petch";
+//        int myInt = 50;
+//        double myDouble = 1000;
+//
+//        System.out.printf("%b \n", myBoolean);
+//        System.out.printf("%c \n", myChar);
+//        System.out.printf("%s \n", myString);
+//        System.out.printf("%d \n", myInt);
+//        System.out.printf("%f \n", myDouble);
+//
+//        //[width]
+//        // minimum number of characters to be written as output
+//        System.out.printf("Hello %-10s \n", myString);
+//
+//        //[precision]
+//        // sets number of digits of precision when outputting floating-point values
+//        System.out.printf("You have this much money %.1f \n", myDouble);
+//
+//        // [flags]
+//        // adds an effect to output based on the flag added to format specifier
+//        // - : left-justify
+//        // + : output a plus ( + ) or minus ( - ) sign for a numeric value
+//        // 0 : numeric values are zero-padded
+//        // , : comma grouping separator if numbers > 1000
+//
+//        System.out.printf("You have this much money %,f \n", myDouble);
+
+// ------------------ Final Keyword -----------------------//
+//        final double PI = 3.14159;
+//        System.out.println(PI);
+//
+//
     }
 
     static void hello(String name, int age) {
