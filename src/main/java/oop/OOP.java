eb361302457c628c0c989b9d34357c0d2ab3c56e
kittichanr <petchkubbb@gmail.com>
2023-08-23 18:13:24 +0700

    oop

--- src/main/java/oop/OOP.java
+++ src/main/java/oop/OOP.java
@@ -1,7 +1,17 @@
 package oop;
 
-public class oop {
+public class OOP {
     public static void main(String[] args) {
-        System.out.println("oop");
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+
+        System.out.println(myCar1.model);
+        System.out.println(myCar1.make);
+        System.out.println();
+        System.out.println(myCar2.model);
+        System.out.println(myCar2.make);
+
+        myCar1.drive();
+        myCar1.brake();
     }
 }
