ad5b98228cddb55ea0d573f4b19db95b2daccfc7
kittichanr <petchkubbb@gmail.com>
2023-08-24 18:15:27 +0700

    constructor

---
+++ src/main/java/constructors/Main.java
@@ -0,0 +1,15 @@
+package constructors;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human1 = new Human("Petch", 26, 63.3);
+        Human human2 = new Human("Boon", 27, 58.9);
+
+        System.out.println(human1.name);
+        System.out.println(human2.name);
+
+        human1.eat();
+        human2.drink();
+
+    }
+}
