534cbf67a065775abbc67de0384b285132e623f5
kittichanr <petchkubbb@gmail.com>
2023-08-26 06:42:54 +0700

    metod overriding

---
+++ src/main/java/oop/method/overriding/Animal.java
@@ -0,0 +1,9 @@
+package oop.method.overriding;
+
+public class Animal {
+
+    void speak() {
+        System.out.println("The animal speaks");
+    }
+
+}
---
+++ src/main/java/oop/method/overriding/Dog.java
@@ -0,0 +1,8 @@
+package oop.method.overriding;
+
+public class Dog extends Animal {
+    @Override
+    void speak() {
+        System.out.println("The dog goes \"bark\"");
+    }
+}
