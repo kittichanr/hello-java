a0b91178ea4a825b1d937fff91cf0fc7ccf8a55c
kittichanr <petchkubbb@gmail.com>
2023-08-27 06:47:56 +0700

    audio

---
+++ src/main/java/audio/Main.java
@@ -0,0 +1,46 @@
+package audio;
+
+import javax.sound.sampled.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("flying.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        String response = "";
+
+        while (!response.equals("Q")) {
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.println("Enter you choice");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"):
+                    clip.start();
+                    break;
+                case ("S"):
+                    clip.stop();
+                    break;
+                case ("R"):
+                    clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"):
+                    clip.close();
+                    break;
+                default:
+                    System.out.println("Not a valid response");
+            }
+        }
+        System.out.println("Byeee!");
+    }
+}
