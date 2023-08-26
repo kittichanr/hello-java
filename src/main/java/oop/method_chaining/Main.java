70408c5eae15c8fc466cd8d27b3020b548bdfb28
kittichanr <petchkubbb@gmail.com>
2023-08-27 06:56:49 +0700

    method chainning

---
+++ src/main/java/oop/method_chaining/Main.java
@@ -0,0 +1,19 @@
+package oop.method_chaining;
+
+public class Main {
+    public static void main(String[] args) {
+        // method chaining =  a common syntax for invoking multiple method calls in OOP
+        //      condense code into less lines
+
+
+        String name = "   petch";
+
+//        name = name.concat(" code    ");
+//        name = name.toUpperCase();
+//        name = name.trim();
+
+        name = name.concat(" code    ").toUpperCase().trim();
+
+        System.out.println(name);
+    }
+}
