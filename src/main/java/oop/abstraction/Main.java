d7e1b7f0c5e2b111f489dd432d915d374b0e3c77
kittichanr <petchkubbb@gmail.com>
2023-08-26 07:00:26 +0700

    abstract

---
+++ src/main/java/oop/abstraction/Main.java
@@ -0,0 +1,12 @@
+package oop.abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        // abstract =  	abstract classes cannot be instantiated, but they can have a subclass
+        //				abstract methods are declared without an implementation
+
+//        Vehicle vehicle = new Vehicle(); // cannot declare to new object because abstract
+        Car car = new Car();
+        car.go();
+    }
+}
