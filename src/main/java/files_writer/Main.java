759cc4881a4102709384119b9978f95364d9f4c1
kittichanr <petchkubbb@gmail.com>
2023-08-26 13:53:49 +0700

    file writer

---
+++ src/main/java/files_writer/Main.java
@@ -0,0 +1,18 @@
+package files_writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("poem.txt");
+            writer.write("Roses are red \nViolets are blue \ntest test test");
+            writer.append("\n(A poem by Petch)");
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
